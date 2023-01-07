0dd6bb1369e5c29387e5af928d1331353a472286
Gustavo Henrique <110679992+gustaaragao@users.noreply.github.com>
2023-01-07 14:41:04 -0300

    Lecture 04  UI (#5)
    * feat: creating-the-game-map
    
    * feat: new-function-to-find-pacman
    
    * feat: new-function-to-move-pacman
    
    * feat: add-UI

---
+++ UI.py
@@ -0,0 +1,6 @@
+def ui_print(map):
+    for row in map:
+        for column in row:
+            print(column, end='')
+            
+        print("")
--- pacman.py
+++ pacman.py
@@ -1,3 +1,5 @@
+from UI import ui_print
+
 # The game map
 # | and - (pipes and dashes) ->  Walls
 # G -> Ghosts
@@ -33,12 +35,11 @@ def move_pacman(map, next_pacman_x, next_pacman_y):
     pacman_x, pacman_y = find_pacman(map)
 
     # The place where the pacman was is now empty.
-    new_row = map[pacman_x][0:pacman_y] + "." + map[pacman_x][pacman_y+1]
+    new_row = map[pacman_x][0:pacman_y] + "." + map[pacman_x][pacman_y+1:]
     # new_row = everything before pacman + empty space + everything after pacman
     map[pacman_x] = new_row
     
     # The new place has the pacman.
-    new_row_2 = map[next_pacman_x][0:next_pacman_y] + "@" + map[next_pacman_x][next_pacman_y+1]
+    new_row_2 = map[next_pacman_x][0:next_pacman_y] + "@" + map[next_pacman_x][next_pacman_y+1:]
     # new_row = everything before empty space + pacman + everything after empty space
     map[next_pacman_x] = new_row_2
-        
