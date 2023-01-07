d45898c40c47b248ec79abe59381b6faa7497133
Gustavo Henrique <110679992+gustaaragao@users.noreply.github.com>
2023-01-07 14:27:40 -0300

    Lecture 03  moving pacman (#4)
    * feat: creating-the-game-map
    
    * feat: new-function-to-find-pacman
    
    * feat: new-function-to-move-pacman

--- pacman.py
+++ pacman.py
@@ -26,3 +26,19 @@ def find_pacman(map):
                 pacman_y = y
     
     return pacman_x, pacman_y
+
+
+# Moving Pacman
+def move_pacman(map, next_pacman_x, next_pacman_y):
+    pacman_x, pacman_y = find_pacman(map)
+
+    # The place where the pacman was is now empty.
+    new_row = map[pacman_x][0:pacman_y] + "." + map[pacman_x][pacman_y+1]
+    # new_row = everything before pacman + empty space + everything after pacman
+    map[pacman_x] = new_row
+    
+    # The new place has the pacman.
+    new_row_2 = map[next_pacman_x][0:next_pacman_y] + "@" + map[next_pacman_x][next_pacman_y+1]
+    # new_row = everything before empty space + pacman + everything after empty space
+    map[next_pacman_x] = new_row_2
+        
