6d55210d1a11a45152b0d5913978ab3dd74bc40d
Gustavo Henrique <110679992+gustaaragao@users.noreply.github.com>
2023-01-07 13:01:05 -0300

    feat: creating-the-game-map (#1)

---
+++ pacman.py
@@ -0,0 +1,16 @@
+# The game map
+# | and - (pipes and dashes) ->  Walls
+# G -> Ghosts
+# . -> Empty spaces where the characteres can walk
+# P -> Pills
+# @ -> Pacman
+
+map = [
+    "|--------|",
+    "|G..|..G.|",
+    "|...PP...|",
+    "|G....@|.|",
+    "|........|",
+    "|--------|"
+]
+
